cbb71e254c180fc9b9728a03bf30985bce36a644
Yuki Liu <im.yukiliu@gmail.com>
2020-12-30 19:17:09 -0600

    update project path

--- org.ekstazi.core/src/main/java/org/ekstazi/changelevel/Macros.java
+++ org.ekstazi.core/src/main/java/org/ekstazi/changelevel/Macros.java
@@ -6,16 +6,16 @@ import java.util.Set;
 public class Macros {
     static HashMap<String, String> projectList = new HashMap<>();
     static{
-        projectList.put("apache/commons-beanutils", "893b769d");
+//        projectList.put("apache/commons-beanutils", "893b769d");
 //        projectList.put("apache/commons-codec", "24059898");
-        projectList.put("apache/commons-compress", "dfa9ed37");
-        projectList.put("apache/commons-pool", "b6775ade");
+//        projectList.put("apache/commons-compress", "dfa9ed37");
+//        projectList.put("apache/commons-pool", "b6775ade");
         projectList.put("alibaba/fastjson", "4c516b12");
-        projectList.put("apache/commons-email", "4f45556f");
-        projectList.put("apache/commons-math", "649b134f");
+//        projectList.put("apache/commons-email", "4f45556f");
+//        projectList.put("apache/commons-math", "649b134f");
     }
     static String home = System.getProperty("user.home");
-    static String projectFolder = "projects/ctaxonomy/ekstazi";
+    static String projectFolder = "projects/ctaxonomy";
     static String projectFolderPath = home + "/" + projectFolder;
     static String resultFolder = "results";
     static String resultFolderPath = projectFolderPath + "/" + resultFolder;
